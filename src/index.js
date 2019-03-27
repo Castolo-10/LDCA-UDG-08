23ee126a3dc44a3f5cc485a2c6e4a4038d8b629d
Castolo-10 <leonardo.castolo@alumnos.udg.mx>
2019-03-27 01:50:42 -0600

    Agregados estilos y finalizado el borrado de productos en mainWindow

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 
 const url = require('url');
 const path = require('path');
 
 if(process.env.NODE_ENV !== 'production'){
   require('electron-reload')(__dirname, {
-
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
   })
 }
 
 
 let mainWindow
+let newProductWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -19,4 +20,87 @@ app.on('ready', () => {
     protocol: 'file',
     slashes: true
   }));
+
+const mainMenu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(mainMenu);
+
+mainWindow.on('closed', () => {
+  app.quit();
+})
+
 });
+
+function createNewProductWindow() {
+  newProductWindow = new BrowserWindow({
+    width: 400,
+    height: 330,
+    title: 'Add a new product'
+  });
+
+  // newProductWindow.setMenu(null);
+
+  newProductWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/new-product.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  newProductWindow.on('closed', () => {
+    newProductWindow = 'null';
+  });
+
+}
+
+ipcMain.on('product:new', (e, newProduct) =>{
+  mainWindow.webContents.send('product:new', newProduct);
+  newProductWindow.close();
+})
+
+const templateMenu = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Product',
+        accelerator: 'Ctrl+N',
+        click(){
+          createNewProductWindow()
+        }
+      },
+      {
+        label: 'Remove All Products',
+      },
+      {
+        label: 'Exit',
+        accelerator: process.platform == 'darwin' ? 'command=Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  },
+];
+
+if(process.platform === 'darwin'){
+  templateMenu.unshift({
+    label: app.getName()
+  })
+}
+
+if(process.env.NODE_ENV !== 'production'){
+  templateMenu.push({
+    label: 'DevTools',
+    submenu: [
+      {
+        label: 'Show/Hide Dev Tools',
+        accelerator: 'Ctrl+Shift+C',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
